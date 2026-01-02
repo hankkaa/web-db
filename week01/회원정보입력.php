9837d2dc0147556157e6a0f5b069d81bb66d8fcd
Hankkaa <gah0519@naver.com>
2026-01-02 21:51:42 +0900

    1회차: 웹의 구조와 HTML 이해

---
+++ week01/회원정보입력.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>모각코 1회차</title>
+</head>
+<body>
+  <h1>회원 정보 입력</h1>
+
+  <form>
+    이름: <input type="text"> <br><br>
+    학번: <input type="text"> <br><br>
+    <button>제출</button>
+  </form>
+  
+</body>
+</html>
